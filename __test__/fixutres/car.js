62cd4e38ce2c4e12a264b5802da88d1ca4416200
Kacper Wdowik <wdowikkacper@gmail.com>
2020-11-13 20:03:10 +0100

    test: add test config

---
+++ __test__/fixutres/car.js
@@ -0,0 +1,36 @@
+import faker from 'faker'
+import cuid from 'cuid'
+import crypto from 'crypto'
+
+const Id = Object.freeze({
+  makeId: cuid,
+  isValidId: cuid.isCuid
+})
+
+function md5 (text) {
+  return crypto
+    .createHash('md5')
+    .update(text, 'utf-8')
+    .digest('hex')
+}
+
+export default function makeFakeCar (overrides) {
+  const car = {
+    id: Id.makeId(),
+    model: faker.vehicle.model(),
+    price: faker.random.number({ min: 30, max: 200, precision: 1 }),
+    available: faker.random.boolean(),
+    category: [ faker.random.arrayElements(['normal', 'hybrid', 'eletric' ,'premium'])]
+  }
+
+  car.hash = md5(
+    (car.id || '') +
+    (car.model || '') +
+    (car.price || '')
+  )
+
+  return {
+    ...car,
+    ...overrides
+  }
+}
